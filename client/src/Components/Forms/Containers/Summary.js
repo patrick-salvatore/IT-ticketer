528aceaefa90a4b0a46405232bddb4d5b75c2e85
patrick-salvatore <salvatorepat8@gmail.com>
2019-05-21 21:56:18 -0400

    small clean

--- client/src/Components/Forms/Containers/Summary.js
+++ client/src/Components/Forms/Containers/Summary.js
@@ -51,29 +51,29 @@ class Summary extends Component {
                 <div className = 'col-12'>
                   <Input title = "Address:" readOnly value = { this.state.Report.owner_add }></Input>
                   <label className = 'font-weight-bold'> 1. Which IT systems, equipment or devices are involved in the security breach?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q1 } />
+                    <Input readOnly value = { this.state.Report.q1 } />
                   <label className = 'font-weight-bold'>2. What information has been lost or compromised?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q2 } />
+                    <Input readOnly value = { this.state.Report.q2 } />
                   <label className = 'font-weight-bold'>3. How much information has been lost?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q3 } />
+                    <Input readOnly value = { this.state.Report.q3 } />
                   <label className = 'font-weight-bold'>4. Is the information Client Based?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q4 } />
+                    <Input readOnly value = { this.state.Report.q4 } />
                   <label className = 'font-weight-bold'>5. If the incident involves the loss of a laptop or portable device, did the device contain Client Data. Was the information, backed up onto Druva systems?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q5 }/>
+                    <Input readOnly value = { this.state.Report.q5 }/>
                   <label className = 'font-weight-bold'>6. Does it include records of operational, legal or evidential value to the HealthSTAR or its Clients?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q6}/>
+                    <Input readOnly value = { this.state.Report.q6}/>
                   <label className = 'font-weight-bold'>7. Is it business-critical? Do users rely on access to this information asset or can they use reliable electronic copies or alternative manual processes e.g. paper files if the information asset is unavailable?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q7}/>
+                    <Input readOnly value = { this.state.Report.q7}/>
                   <label className = 'font-weight-bold'>8. How urgently would access need to be restored to an information asset to resume business or, to return to the required standard of service?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q8}/>
+                    <Input readOnly value = { this.state.Report.q8}/>
                   <label className = 'font-weight-bold'>9. Will the loss or compromise of the information have adverse operational, financial legal, liability or reputational consequences for HealthSTAR or its Clients?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q9}/>
+                    <Input readOnly value = { this.state.Report.q9}/>
                   <label className = 'font-weight-bold'>10. Is the information bound by any contractual security arrangements e.g. to Clients?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q10}/>
+                    <Input readOnly value = { this.state.Report.q10}/>
                   <label className = 'font-weight-bold'>11. Is any of the information confidential?</label>
-                    <Input title = '' readOnly value = { this.state.Report.q11}/>
+                    <Input readOnly value = { this.state.Report.q11}/>
                   <label className = 'font-weight-bold'>12. High Risk Action </label>
-                    <TextArea title = '' readOnly value = { this.state.Report.action}/>
+                    <TextArea readOnly value = { this.state.Report.action}/>
                 </div>
               </div>
           </div>
